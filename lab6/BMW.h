213d2f5e7ac5de0bbbbcc8a7391d0736a1733357
Goldan David Teodor <64546161+GoldanDavidTeodor@users.noreply.github.com>
2024-04-09 17:57:36 +0300

    Add files via upload

---
+++ lab6/BMW.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Car.h"
+
+
+class BMW : public Car 
+{
+public:
+	BMW();
+	float calculateTime(int, int, bool&) override;
+	const char* GetName() override;
+};
+
---
+++ lab6/Seat.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Car.h"
+
+
+class Seat : public Car
+{
+public:
+	Seat();
+	float calculateTime(int, int, bool&) override;
+	const char* GetName() override;
+};
+
